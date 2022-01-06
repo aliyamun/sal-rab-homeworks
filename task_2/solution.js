68ac80f8d93864ffae984d2d65cdfb0aeec988d1
unknown <avyahina@gmail.com>
2022-01-06 17:39:06 +0400

    task_2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,18 +6,10 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     let shippingSum; // создайте переменную shippingSum
-    If (productsSum = 0) {
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
-    }
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
-    if (productsSum >= freeShippingMinSum) {
-        shippingSum = 0;
-    }
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
-    if (productsSum > 0) && (productsSum < freeShippingMinSum) {
-        shippingSum = shippingPrice
+      } else {
+           shippingSum = shippingPrice
     }
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
@@ -35,8 +27,8 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     let discountSum; // создайте переменную discountSum
-    if (productsSum >= discountSum) {
-        discountSum = (discountPart / productsSum) * 100;
+    if (productsSum >= discountMinSum) {
+        discountSum = (productsSum / discountPart);
     } else {
         discountSum = 0;
     }
